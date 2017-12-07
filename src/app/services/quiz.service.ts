4ca0d06d4627e15c382387cbef76196c2b4bb370
InteractivePie <nickvdlaan@gmail.com>
2017-12-07 13:44:04 +0100

    Added models for quizes, service logic, and start for frontend

---
+++ src/app/services/quiz.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class QuizService {
+
+  constructor(private http: Http) { }
+
+  get(url: string) {
+    return this.http.get(url).map(res => res.text().length > 0 ? res.json() : null);
+  }
+
+  getAll() {
+    return [
+      { id: 'assets/quizen/schijfvanvijf.json', name: 'De Schijf van Vijf' },
+      { id: 'assets/quizen/vetten.json', name: 'Vetten' },
+      { id: 'assets/quizen/koolhydraten.json', name: 'Koolhydraten' }
+    ];
+  }
+
+}
