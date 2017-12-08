db8eb33cb67de0a9864b09f13c086735214172ea
NickvdLaan <nickvdlaan@gmail.com>
2017-12-08 12:17:41 +0100

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { QuizComponent } from './quiz/quiz.component';
+
+const appRoutes: Routes = [
+  { path: 'quiz', component: QuizComponent }
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  //{ path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -14,7 +31,11 @@ import { QuizComponent } from './quiz/quiz.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+     appRoutes,
+     { enableTracing: true } // <-- debugging purposes only
+   )
   ],
   providers: [],
   bootstrap: [AppComponent]
