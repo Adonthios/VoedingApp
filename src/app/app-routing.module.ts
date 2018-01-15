d54d24b4c0978ec5541bb8c5d9a075750b441a4f
NickvdLaan <nickvdlaan@gmail.com>
2018-01-15 09:27:37 +0100

    Added ranklist url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {UserLoginComponent} from './auth/user-login/user-login.component';
+import {RanklistComponent} from './ranklist/ranklist.component';
 import {QuizComponent} from './quiz/quiz.component';
 import {UserInfoComponent} from './auth/user-info/user-info.component';
 
@@ -10,6 +11,7 @@ const routes: Routes = [
   {path: 'quiz', component: QuizComponent },
   {path: 'login', component: UserLoginComponent},
   {path: 'user', component: UserInfoComponent},
+  {path: 'ranklist', component: RanklistComponent }
 ];
 
 @NgModule({
