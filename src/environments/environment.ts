d5ce19b95a8667f8b5394902a1fb074710883df3
InteractivePie <nickvdlaan@gmail.com>
2018-01-16 17:21:00 +0100

    updated app based on feedback

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,20 +5,20 @@
 
 export const environment = {
   production: false,
+  // firebase: {
+  //   apiKey: "AIzaSyDJYAItIOTcgPUF4Etk4mfVO9v-V_NfGcg",
+  //   authDomain: "voedingquiz.firebaseapp.com",
+  //   databaseURL: "https://voedingquiz.firebaseio.com",
+  //   projectId: "voedingquiz",
+  //   storageBucket: "",
+  //   messagingSenderId: "185770240032"
+  // }
   firebase: {
-    apiKey: "AIzaSyDJYAItIOTcgPUF4Etk4mfVO9v-V_NfGcg",
-    authDomain: "voedingquiz.firebaseapp.com",
-    databaseURL: "https://voedingquiz.firebaseio.com",
-    projectId: "voedingquiz",
-    storageBucket: "",
-    messagingSenderId: "185770240032"
-  }
- //  firebase: {
- //   apiKey: "AIzaSyDnLg14LNoWAbcDRnr_V95ClZRIEUlapLE",
- //   authDomain: "voedingapp.firebaseapp.com",
- //   databaseURL: "https://voedingapp.firebaseio.com",
- //   projectId: "voedingapp",
- //   storageBucket: "",
- //   messagingSenderId: "1029133652786"
- // }
+   apiKey: "AIzaSyDnLg14LNoWAbcDRnr_V95ClZRIEUlapLE",
+   authDomain: "voedingapp.firebaseapp.com",
+   databaseURL: "https://voedingapp.firebaseio.com",
+   projectId: "voedingapp",
+   storageBucket: "",
+   messagingSenderId: "1029133652786"
+ }
 };
